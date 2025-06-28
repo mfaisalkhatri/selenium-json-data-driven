71003610aa8f561bc57b9c6c64ba883aa68d0249
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2025-06-28 20:01:14 +0300

    appended timestamp in the filename to get unique screenshot name every time

--- src/test/java/io/github/mfaisalkhatri/utils/ScreenshotListener.java
+++ src/test/java/io/github/mfaisalkhatri/utils/ScreenshotListener.java
@@ -4,6 +4,8 @@ import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
@@ -16,14 +18,17 @@ public class ScreenshotListener implements ITestListener {
     public void onTestFailure (final ITestResult result) {
         Object testClass = result.getInstance ();
         WebDriver driver = ((WebDriverProvider) testClass).getDriver ();
+        String timestamp = new SimpleDateFormat ("yyyyMMdd_HHmmss_SSS").format (new Date ());
+
 
         if (driver != null) {
             String testName = result.getMethod ()
                 .getMethodName ();
             File screenshot = ((TakesScreenshot) driver).getScreenshotAs (OutputType.FILE);
+            String filename = timestamp + ".png";
             try {
                 Files.createDirectories (Paths.get ("screenshots"));
-                Files.copy (screenshot.toPath (), Paths.get ("screenshots", testName+ ".png"));
+                Files.copy (screenshot.toPath (), Paths.get (testName, filename));
             } catch (IOException e) {
                 throw new RuntimeException (e);
             }
