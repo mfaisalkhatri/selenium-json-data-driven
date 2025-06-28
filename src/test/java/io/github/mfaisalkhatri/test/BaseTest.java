00525f334544ee12a861ed9f682b05bb99dafb41
mohammadfaisalkhatri@gmail.com <mohammadfaisalkhatri@gmail.com>
2025-06-28 19:14:34 +0300

    added chrome option to use disable-link-features

--- src/test/java/io/github/mfaisalkhatri/test/BaseTest.java
+++ src/test/java/io/github/mfaisalkhatri/test/BaseTest.java
@@ -21,6 +21,7 @@ public class BaseTest implements WebDriverProvider {
     @BeforeClass
     public void setup () {
         ChromeOptions chromeOptions = new ChromeOptions ();
+        chromeOptions.addArguments("--disable-blink-features=AutomationControlled");
         chromeOptions.addArguments ("--headless=new", "--no-sandbox", "--window-size=1920,1080");
 
         this.driver = new ChromeDriver (chromeOptions);
